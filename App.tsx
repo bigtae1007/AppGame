1ef2395f9523c5e2602f20f8f62825f191fabf4c
taehyeong park <b_tae96@naver.com>
2022-09-13 00:05:51 +0900

    section4/49~50  - styling input&button

--- App.tsx
+++ App.tsx
@@ -1,23 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import {StyleSheet, View} from 'react-native';
-
 import StartGameScreen from './screens/StartGameScreen';
 
 const App = () => {
-  return (
-    <View>
-      <StartGameScreen />
-    </View>
-  );
+  return <StartGameScreen />;
 };
 
 const styles = StyleSheet.create({});
--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import {TextInput, Button, View} from 'react-native';
+import {TextInput, View, StyleSheet} from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 export default function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: '#72063c',
+    borderRadius: 8,
+    // android 그림자 영역
+    elevation: 4,
+    // IOS 그림자
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    /*<----------------------------------------> */
+  },
+  numberInput: {
+    height: 60,
+    width: 50,
+    fontSize: 32,
+    borderBottomWidth: 2,
+    borderBottomColor: '#ddb52f',
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
